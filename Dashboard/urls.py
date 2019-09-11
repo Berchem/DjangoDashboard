4ffd900e0b46f2b98f7d6eb1d7aaf189096fb0ff
Berchem <34000467+Berchem@users.noreply.github.com>
2019-09-11 19:15:16 +0800

    [UPDATE] Dashboard: add SystemUtil

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from Futures import views
 
 urlpatterns = [
@@ -26,5 +26,6 @@ urlpatterns = [
     url('^elements/$', views.elements),
     url('^panels/$', views.panels),
     url('^login/$', views.login),
+    url(r'^system/', include('SystemUtil.urls'))
 
 ]
