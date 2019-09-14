7752730dbc62f1ece00b8b9b536a4600086655a6
Berchem <low118profile@gmail.com>
2019-09-15 00:40:50 +0800

    [UPDATE] Dashboard: Widgets: setInterval

--- static/js/widgets/widgets.js
+++ static/js/widgets/widgets.js
@@ -1,4 +1,8 @@
-var system_status = setInterval("load('system_status')","3000");
+
+var system_status_update_period = "3000"
+var system_status = setInterval("load('system_status')", system_status_update_period);
+
+var chat_status_update_period = "1500"
 var chat_status = setInterval("load('chat_status')","1500");
 var timeout;
 
@@ -6,7 +10,7 @@ $(".dropdown").on("click", function(){
     clearInterval(system_status)
     clearInterval(chat_status)
     setTimeout(function(){
-        system_status = setInterval("load('system_status')","3000");
-        chat_status = setInterval("load('chat_status')","1500");
+        system_status = setInterval("load('system_status')", system_status_update_period);
+        chat_status = setInterval("load('chat_status')", chat_status_update_period);
     }, 10000)
 })
