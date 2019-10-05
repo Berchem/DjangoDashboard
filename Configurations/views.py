a2212bc2a582def64e38df5107b639c30011b86f
Berchem <low118profile@gmail.com>
2019-10-06 03:26:35 +0800

    [UPDATE] Config: add md5

--- Configurations/views.py
+++ Configurations/views.py
@@ -1,8 +1,10 @@
+from md5 import md5
+
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+from django.shortcuts import render
 
 from Configurations.models import CapitalAccount
 from Configurations.models import CapitalFuturesCorp
@@ -30,8 +32,10 @@ def check(request):
 def update_capital_account(request):
     capital = CapitalAccount.objects.get(username=request.user.username)
     capital.account = request.POST["capital_account"]
-    capital.password = request.POST["capital_password"]
-    capital.id_number = request.POST["id_number"].upper()
+    if request.POST["capital_password"] != capital.password:
+        capital.password = md5(request.POST["capital_password"])
+    if request.POST["id_number"] != capital.password:
+        capital.id_number = md5(request.POST["id_number"].upper())
     capital.deposit_code = request.POST["deposit_code"]
     capital.deposit_account = request.POST["deposit_account"].zfill(16)
     capital.save()
