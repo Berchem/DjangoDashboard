8682ce9ab43229f92f0bbc31a5afd897150b2c2f
Berchem <low118profile@gmail.com>
2019-10-06 03:29:22 +0800

    [UPDATE] Futures: combine quote result from api

--- Futures/models.py
+++ Futures/models.py
@@ -12,3 +12,49 @@ class HModel(models.Model):
     class Meta:
         managed = False
         db_table = 'h_model'
+
+
+class TestKLineData(models.Model):
+    index = models.IntegerField(primary_key=True)
+    item_code = models.TextField(blank=True, null=True)
+    datetime = models.TextField(blank=True, null=True)
+    open = models.TextField(blank=True, null=True)
+    high = models.TextField(blank=True, null=True)
+    low = models.TextField(blank=True, null=True)
+    close = models.TextField(blank=True, null=True)
+    volume = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'test_k_line_data'
+
+
+class TestQuoteData(models.Model):
+    index = models.IntegerField(primary_key=True)
+    item_code = models.TextField(blank=True, null=True)
+    datetime = models.TextField(blank=True, null=True)
+    open = models.TextField(blank=True, null=True)
+    high = models.TextField(blank=True, null=True)
+    low = models.TextField(blank=True, null=True)
+    close = models.TextField(blank=True, null=True)
+    volume = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'test_quote_data'
+
+
+class TestTicksData(models.Model):
+    index = models.IntegerField(primary_key=True)
+    sstockidx = models.TextField(db_column='sStockIdx', blank=True, null=True)  # Field name made lowercase.
+    nptr = models.TextField(db_column='nPtr', blank=True, null=True)  # Field name made lowercase.
+    datetime = models.TextField(blank=True, null=True)
+    nbid = models.TextField(db_column='nBid', blank=True, null=True)  # Field name made lowercase.
+    nask = models.TextField(db_column='nAsk', blank=True, null=True)  # Field name made lowercase.
+    nclose = models.TextField(db_column='nClose', blank=True, null=True)  # Field name made lowercase.
+    nqty = models.TextField(db_column='nQty', blank=True, null=True)  # Field name made lowercase.
+    nsimulate = models.TextField(db_column='nSimulate', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'test_ticks_data'
--- Futures/views.py
+++ Futures/views.py
@@ -1,27 +1,69 @@
+import datetime as dt
+
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from Widgets.views import get_to_do_list
 from Futures.models import HModel
-
-
-# Create your views here.
-def redirect(request):
-    title = "Dashboard"
-    to_do_list = get_to_do_list(request)
-    return render(request, 'index.html', locals())
-
-
-def index(requests):
-    return render(requests, 'index.html', locals())
+from Futures.models import TestKLineData
+from Futures.models import TestQuoteData
 
 
 @login_required
 def futures(request):
     title = "Futures"
+    tsea_today_close = TestKLineData.objects.\
+        filter(item_code='TSEA').\
+        order_by('-index')[0]
+
+    today = dt.datetime.today()
+    close = today.replace(hour=13, minute=30, second=0, microsecond=0)
+    today = close if today > close else close - dt.timedelta(days=1)
+
+    tsea_quote = TestQuoteData.objects.\
+        filter(item_code='TSEA'). \
+        order_by('-datetime')[0]
+        # filter(datetime__startswith=today.strftime("%Y/%m/%d %H:%M")).\
+        # order_by('-datetime')[0]
+
+    mtx_current_quote = TestQuoteData.objects.\
+        filter(item_code='MTX00').\
+        order_by('-datetime')[0]
+
+    mtx_next_quote = TestQuoteData.objects. \
+        filter(item_code='MTX%02d' % (today.month + 1)). \
+        order_by('-datetime')[0]
     return render(request, 'futures.html', locals())
 
+
+def get_quote(request):
+    tsea_today_close = TestKLineData.objects. \
+        filter(item_code='TSEA'). \
+        order_by('-index')[0]
+
+    today = dt.datetime.today()
+    close = today.replace(hour=13, minute=30, second=0, microsecond=0)
+    today = close if today > close else close - dt.timedelta(days=1)
+
+    mtx_current = TestKLineData.objects. \
+        filter(item_code='MTX00'). \
+        filter(datetime__startswith=today.strftime("%Y/%m/%d %H:%M")). \
+        order_by('-datetime')[0]
+    mtx_current = {'item_code': mtx_current.item_code}
+
+    mtx_next = TestKLineData.objects. \
+        filter(item_code='MTX%02d' % (today.month + 1)). \
+        filter(datetime__startswith=today.strftime("%Y/%m/%d %H:%M")). \
+        order_by('-datetime')[0]
+
+    mtx_current_quote = TestQuoteData.objects. \
+        filter(item_code='MTX00'). \
+        order_by('-datetime')[0]
+
+    mtx_next_quote = TestQuoteData.objects. \
+        filter(item_code='MTX%02d' % (today.month + 1)). \
+        order_by('-datetime')[0]
+    return
 
 def get_data(request):
     data = []
@@ -35,18 +77,3 @@ def get_data(request):
         row["next_price"] = int(record.next_price)
         data += [row]
     return JsonResponse(data, safe=False)
-
-
-def charts(requests):
-    title = "Charts"
-    return render(requests, 'charts.html', locals())
-
-
-def elements(requests):
-    title = "UI Elements"
-    return render(requests, 'elements.html', locals())
-
-
-def panels(requests):
-    title = "Panels"
-    return render(requests, 'panels.html', locals())
